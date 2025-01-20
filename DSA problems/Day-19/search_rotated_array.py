b832cb53ad5bb264e3abc9de0b13cc271c9305b8
Deepak Ravikumar <deepak.060593@gmail.com>
2025-01-20 15:31:53 +0530

    day-19-binary-search-easy

---
+++ DSA problems/Day-19/search_rotated_array.py
@@ -0,0 +1,16 @@
+# leetcode
+# 33. Search in Rotated Sorted Array
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        for i in range(n):
+            if nums[i]==target:
+                return i
+        return -1
+        
