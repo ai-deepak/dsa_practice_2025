141524a5d00cb0c9e5dd52bfbf49b68f1f037338
Deepak Ravikumar <deepak.060593@gmail.com>
2025-01-21 18:48:03 +0530

    Day-20-array-hard-majority-element

--- DSA problems/Day-20/majority_element.py
+++ DSA problems/Day-20/majority_element.py
@@ -1,7 +1,7 @@
-nums = [2,2]
-n = len(nums)
-# greater than, so we add +1
-minCount = n//3 + 1
+# nums = [2,2]
+# n = len(nums)
+# # greater than, so we add +1
+# minCount = n//3 + 1
 
 #brute force (time O(n^2) and space O(n))
 # check=[]
@@ -40,4 +40,39 @@ minCount = n//3 + 1
 
 # print(better(nums,n,minCount))
 
-#optimal (time O(n) and space O(n))
+#optimal (time O(n) and space O(1)) Moore's Voting Algorithm
+
+def optimal(nums,n,minCount):
+    counter1=0
+    counter2=0
+    ele1=0
+    ele2=0
+    res=[]
+    for i in range(n):
+        if counter1==0 and nums[i]!=ele2:
+            counter1=1
+            ele1=nums[i]
+        elif counter2==0 and nums[i]!=ele1:
+            counter2=1
+            ele2=nums[i]
+        elif ele1==nums[i]:
+            counter1+=1
+        elif ele2==nums[i]:
+            counter2+=1
+        else:
+            counter1-=1
+            counter2-=1
+    counter1=0
+    counter2=0
+    for i in range(n):
+        if nums[i]==ele1:
+            counter1+=1
+        elif nums[i]==ele2:
+            counter2+=1
+    if counter1 >= minCount:
+        res.append(ele1)
+    if counter2 >= minCount:
+        res.append(ele2)
+    return res
+
+# print(optimal(nums,n,minCount))
